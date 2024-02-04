15e6cc56f06b308a2eb6a8630968972162f6f9ca
soumik9876 <soumik9876@gmail.com>
2024-02-04 19:54:48 +0600

    Modified user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    class UserTypes(models.TextChoices):
+        SELLER = 'seller', _('SELLER')
+        BUYER = 'buyer', _('BUYER')
+
+    user_type = models.CharField(
+        max_length=10, verbose_name=_('User type'), choices=UserTypes.choices,
+        default=UserTypes.BUYER
+    )
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+    def __str__(self):
+        return self.username
